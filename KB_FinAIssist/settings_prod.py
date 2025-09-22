1028a0a4060adf15c498c59c85004714d4899927
my-cookies-26 <my_cookies_@naver.com>
2025-09-22 21:14:28 +0900

    feat: dashboard 500 에러 수정 체크 및 여신심사 페이지 기본 추가

--- KB_FinAIssist/settings_prod.py
+++ KB_FinAIssist/settings_prod.py
@@ -8,3 +8,22 @@ ALLOWED_HOSTS = [ALLOWED_HOST, ]
 
 # 배포 DB 설정
 # settings.py 에서 가져옴
+
+# import pymysql
+
+# pymysql.install_as_MySQLdb()
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': config.PROB_DB_NAME,
+#         'USER': config.PROB_DB_USER,
+#         'PASSWORD': config.PROB_DB_PASSWORD,
+#         'HOST': config.PROB_DB_HOST,
+#         'PORT': config.PROB_DB_PORT,
+#         'OPTIONS': {
+#             'charset': 'utf8mb4',
+#         },
+#     }
+# }
+# print("MySQL 데이터베이스 설정을 사용합니다.")
