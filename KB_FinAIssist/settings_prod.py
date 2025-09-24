65cca65ff83bccda0e793e43253be74e36df8b88
sumilee <tnal821@gmail.com>
2025-09-24 13:51:50 +0900

    feat: csrf error solve

--- KB_FinAIssist/settings_prod.py
+++ KB_FinAIssist/settings_prod.py
@@ -1,12 +1,16 @@
 from .settings import *   # 베이스 설정 불러오기
 import os
 
-DEBUG = False # 운영환경 디버깅 (임시)
+DEBUG = True # 운영환경 디버깅 (임시)
 
-# 3) CSRF 신뢰 오리진
-_raw_csrf = os.getenv("CSRF_TRUSTED_ORIGINS", "").replace(" ", "")
-CSRF_TRUSTED_ORIGINS = [o for o in _raw_csrf.split(",") if o]
-
+# # CSRF 신뢰 오리진 설정
+# Django 4.0부터 CSRF_TRUSTED_ORIGINS는 'https://' 또는 'http://'와 같은 스킴을 포함해야 합니다.
+CSRF_TRUSTED_ORIGINS = [
+  "https://finaissist.com",
+  "https://www.finaissist.com",
+  "http://FinAissistWeb2-env.eba-ppiwmga8.ap-northeast-2.elasticbeanstalk.com",
+  "https://FinAissistWeb2-env.eba-ppiwmga8.ap-northeast-2.elasticbeanstalk.com",
+]
 # ALLOWED_HOSTS = ['.elasticbeanstalk.com']
 ALLOWED_HOSTS = [
   "FinAissistWeb2-env.eba-ppiwmga8.ap-northeast-2.elasticbeanstalk.com",
